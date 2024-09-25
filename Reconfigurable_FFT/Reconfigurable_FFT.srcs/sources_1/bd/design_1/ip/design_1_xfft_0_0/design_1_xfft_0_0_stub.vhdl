606800bebba31cc6a64534bebc30cf58d6da16a5
jasnoor-sandhu <jas9noor@gmail.com>
2024-09-25 22:48:18 +0530

    Adding Reconfigurable FFT Vivado Project

---
+++ Reconfigurable_FFT/Reconfigurable_FFT.srcs/sources_1/bd/design_1/ip/design_1_xfft_0_0/design_1_xfft_0_0_stub.vhdl
@@ -0,0 +1,48 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+-- Date        : Wed Sep 25 19:06:09 2024
+-- Host        : JASNOORSANDF62C running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/jasnoorsandhu/XILINX_Projects/FFT/Reconfigurable_FFT/Reconfigurable_FFT.srcs/sources_1/bd/design_1/ip/design_1_xfft_0_0/design_1_xfft_0_0_stub.vhdl
+-- Design      : design_1_xfft_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity design_1_xfft_0_0 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    aresetn : in STD_LOGIC;
+    s_axis_config_tdata : in STD_LOGIC_VECTOR ( 23 downto 0 );
+    s_axis_config_tvalid : in STD_LOGIC;
+    s_axis_config_tready : out STD_LOGIC;
+    s_axis_data_tdata : in STD_LOGIC_VECTOR ( 63 downto 0 );
+    s_axis_data_tvalid : in STD_LOGIC;
+    s_axis_data_tready : out STD_LOGIC;
+    s_axis_data_tlast : in STD_LOGIC;
+    m_axis_data_tdata : out STD_LOGIC_VECTOR ( 63 downto 0 );
+    m_axis_data_tvalid : out STD_LOGIC;
+    m_axis_data_tready : in STD_LOGIC;
+    m_axis_data_tlast : out STD_LOGIC;
+    event_frame_started : out STD_LOGIC;
+    event_tlast_unexpected : out STD_LOGIC;
+    event_tlast_missing : out STD_LOGIC;
+    event_status_channel_halt : out STD_LOGIC;
+    event_data_in_channel_halt : out STD_LOGIC;
+    event_data_out_channel_halt : out STD_LOGIC
+  );
+
+end design_1_xfft_0_0;
+
+architecture stub of design_1_xfft_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,aresetn,s_axis_config_tdata[23:0],s_axis_config_tvalid,s_axis_config_tready,s_axis_data_tdata[63:0],s_axis_data_tvalid,s_axis_data_tready,s_axis_data_tlast,m_axis_data_tdata[63:0],m_axis_data_tvalid,m_axis_data_tready,m_axis_data_tlast,event_frame_started,event_tlast_unexpected,event_tlast_missing,event_status_channel_halt,event_data_in_channel_halt,event_data_out_channel_halt";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "xfft_v9_1_3,Vivado 2019.2";
+begin
+end;
